0788547fe7e80e7dd74a52d1b7810a79eebb3aa0
anany-more09 <ananymore63@gmail.com>
2025-04-14 10:20:35 +0530

    added feature

--- Expense-tracker/src/components/dashboard.jsx
+++ Expense-tracker/src/components/dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Input from './input';
 
+
 export default function Dashboard() {
   const [income, setIncome] = useState(0);
   const [expense, setExpense] = useState(0);
@@ -12,18 +13,23 @@ export default function Dashboard() {
     other: 0
   });
   const [selectedCategory, setSelectedCategory] = useState('');
-  const [inputValues, setInputValues] = useState({
-    income: '',
-    expense: ''
-  });
+  // const [inputValues, setInputValues] = useState({
+  //   income: '',
+  //   expense: ''
+  // });
+
+  const[incomeValue, setIncomeValue] = useState('');
+  const[expenseValue, setExpenseValue] = useState('');
+
   const [expenseList, setExpenseList] = useState([]);
+  const [description, setDescription] = useState([]);
 
   function handleClick(e) {
     e.preventDefault();
     const operation = e.target.value;
 
     if (operation === 'income') {
-      const newIncome = income + parseFloat(inputValues.income);
+      const newIncome = income + parseFloat(incomeValue);
       setIncome(newIncome);
       localStorage.setItem("income", newIncome) // Used LocalStorage to store the item 
       setSaving(newIncome - expense);
@@ -31,11 +37,10 @@ export default function Dashboard() {
 
     if (operation === 'expense') {
 
-      const newExpense = expense + parseFloat(inputValues.expense); 
+      const newExpense = expense + parseFloat(expenseValue); 
       if (!selectedCategory) {
         alert("Please select a category");
         return;
-
       }
 
       setExpense(newExpense);
@@ -51,13 +56,13 @@ export default function Dashboard() {
       if (selectedCategory) {
         setCategory(prev => ({
           ...prev,
-          [selectedCategory]: prev[selectedCategory] + parseFloat(inputValues.expense)
+          [selectedCategory]: prev[selectedCategory] + parseFloat(expenseValue)
         }));
          
         if(!isNaN(income-newExpense))
           {
           
-            const item = `${selectedCategory} : ${+(inputValues.expense)}`
+            const item = `${selectedCategory} : ${+(expenseValue)}, Details : ${description}` // Created a list for appending in UL.
             setExpenseList(prev => [...prev, item])
           }
           else{
@@ -67,29 +72,23 @@ export default function Dashboard() {
     }
 
     // Reset input values
-    setInputValues({ income: '', expense: '' });
+    setExpenseValue('');
+    setIncomeValue('')
   }
 
   return (
     <>
     <div className='flex '>
-    <h2 className=' fixed mb-7 bg-amber-200 px-7 pb-2 rounded-xl pt-1 font-bold ml-43 text-green-800 '>Expense Tracker</h2> 
+    <h2 className=' fixed mb-7 bg-amber-200 px-7 pb-2 rounded-xl pt-1 font-bold ml-85 text-green-800 '>Expense Tracker</h2> 
       <div className='border border-amber-300 rounded-2xl py-7 px-5 m-6 '>                      
         <div className=''>
         <div className='pl-22'>
-          <input
-            type="text"
-            placeholder="Income"
-            className='m-2 p-2 border border-purple-400 rounded-2xl'
-            value={inputValues.income}
-            onChange={(e) =>
-              setInputValues((prev) => ({
-                ...prev,
-                income: e.target.value
-              }))
-            }
-          />
-
+          
+         <Input 
+              placeholder={"Income"} 
+              value={incomeValue} 
+              onChange={(e) => setIncomeValue(e.target.value)}
+         />
           <button 
 
               value="income"
@@ -99,10 +98,9 @@ export default function Dashboard() {
             Add Income
           </button>
 
-          
         </div>
 
-        <div className='m-3 my-9 flex md:justify-start'>
+        <div className='m-3 my-9 flex md:justify-center'>
 
           <select
             value={selectedCategory}
@@ -115,18 +113,22 @@ export default function Dashboard() {
             <option value="travel" className='text-black'>Travel</option>
             <option value="other" className='text-black'>Other</option>
           </select>
-          <input
-            type="text"
-            placeholder="Expenses"
-            value={inputValues.expense}
-            onChange={(e) =>
-              setInputValues(prev => ({
-                ...prev,
-                expense: e.target.value
-              }))
-            }
-            className='m-2 p-2 border border-purple-400 rounded-2xl'
-          />
+        
+
+        <Input 
+              placeholder={"Expense"} 
+              value={expenseValue} 
+              onChange={(e) => setExpenseValue(e.target.value)}
+         />
+      
+         <Input 
+
+              placeholder={"Description"}
+              value={description}
+              onChange={(e) => setDescription(e.target.value)
+
+              }
+         />
 
           <button 
               value="expense" 
@@ -135,9 +137,10 @@ export default function Dashboard() {
           >
             Add Expense
           </button>
+  
          </div>
         </div>
-        <div className='border rounded-2xl px-4 flex mt-10 p-3 flex-col md:justify-around md:flex-row'>
+        <div className='border rounded-2xl px-4 flex mt-10 p-3 flex-col md:justify-around md:flex-row w-3xl'>
           <h2 className='text-white font-bold mx-2'>Income:<span className='text-green-600 m-2'>$ {income}</span></h2>
           <h2 className='text-whitefont-bold mx-2'>Expenses:<span className='text-red-500 m-2'>$ {expense}</span></h2>
           <h2 className='text-white font-bold mx-2'>Total Saving:<span className='text-blue-600 m-2'>$ {saving}</span></h2>
@@ -156,14 +159,13 @@ export default function Dashboard() {
         <div className="border rounded-2xl mt-4 m-3 pt-4 px-22 overflow-y-auto h-98">
   <ul>
     {expenseList.map((item, index) => (
-      <li key={index} className="text-red-600 text-left">{item}</li>
+      <li key={index} className="text-red-200 text-left">{item}</li>
     ))}
   </ul>
 </div>
-    
       </div>
     </div>
     </>
   );
 }
-  
+
--- Expense-tracker/src/components/input.jsx
+++ Expense-tracker/src/components/input.jsx
@@ -1,9 +1,18 @@
-export default function Input({amountType})
-{
-    return(
-        <>
-         <input type="text" placeholder={amountType} className='m-2 p-2 border border-purple-400 rounded-2xl' onChange={onchange} />
-        </>
-    );
+import React from 'react'
 
+function Input({placeholder, value, onChange}) {
+  return (
+      <>
+        <input 
+            type="text" 
+            placeholder={placeholder}
+            required 
+            value={value} 
+            onChange={onChange}
+            className='m-2 p-2 border border-purple-400 rounded-2xl'
+        />
+      </>
+  )
 }
+
+export default Input
