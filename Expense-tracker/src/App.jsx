36e78ce1d842eb3c386a2df672d24218420ac081
anany-more09 <ananymore63@gmail.com>
2025-05-01 17:39:28 +0530

    updated

--- Expense-tracker/src/App.jsx
+++ Expense-tracker/src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
-import Dashboard from './components/dashboard'
+// import Dashboard from './components/dashboard'
+import Random from './components/Random'
 
 function App() {
    
   return (
     <>
-    <Dashboard />
+    {/* <Dashboard /> */}
+    <Random />
     </>
   )
 }
